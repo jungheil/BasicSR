aef6a2960787d1b246f488ed227571233b3e775f
jungheil <jungheilai@gmail.com>
2022-04-12 08:57:08 +0800

    fix han

--- basicsr/archs/han_arch.py
+++ basicsr/archs/han_arch.py
@@ -1,22 +1,93 @@
-import torch
-import torch.nn as nn
-from basicsr.utils.registry import ARCH_REGISTRY
-import pdb
 import math
 
-def make_model(args, parent=False):
-    return HAN(args)
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pdb
+from basicsr.utils.registry import ARCH_REGISTRY
+
 
 def default_conv(in_channels, out_channels, kernel_size, bias=True):
     return nn.Conv2d(
-        in_channels, out_channels, kernel_size,
-        padding=(kernel_size//2), bias=bias)
+        in_channels, out_channels, kernel_size, padding=(kernel_size // 2), bias=bias
+    )
 
-class Upsampler_han(nn.Sequential):
+
+class MeanShift(nn.Conv2d):
+    def __init__(
+        self,
+        rgb_range,
+        rgb_mean=(0.4488, 0.4371, 0.4040),
+        rgb_std=(1.0, 1.0, 1.0),
+        sign=-1,
+    ):
+
+        super(MeanShift, self).__init__(3, 3, kernel_size=1)
+        std = torch.Tensor(rgb_std)
+        self.weight.data = torch.eye(3).view(3, 3, 1, 1) / std.view(3, 1, 1, 1)
+        self.bias.data = sign * rgb_range * torch.Tensor(rgb_mean) / std
+        for p in self.parameters():
+            p.requires_grad = False
+
+
+class BasicBlock(nn.Sequential):
+    def __init__(
+        self,
+        conv,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        bias=False,
+        bn=True,
+        act=nn.ReLU(True),
+    ):
+
+        m = [conv(in_channels, out_channels, kernel_size, bias=bias)]
+        if bn:
+            m.append(nn.BatchNorm2d(out_channels))
+        if act is not None:
+            m.append(act)
+
+        super(BasicBlock, self).__init__(*m)
+
+
+class ResBlock(nn.Module):
+    def __init__(
+        self,
+        conv,
+        n_feats,
+        kernel_size,
+        bias=True,
+        bn=False,
+        act=nn.ReLU(True),
+        res_scale=1,
+    ):
+
+        super(ResBlock, self).__init__()
+        m = []
+        for i in range(2):
+            m.append(conv(n_feats, n_feats, kernel_size, bias=bias))
+            if bn:
+                m.append(nn.BatchNorm2d(n_feats))
+            if i == 0:
+                m.append(act)
+
+        self.body = nn.Sequential(*m)
+        self.res_scale = res_scale
+
+    def forward(self, x):
+        res = self.body(x).mul(self.res_scale)
+        res += x
+
+        return res
+
+
+class Upsampler(nn.Sequential):
     def __init__(self, conv, scale, n_feats, bn=False, act=False, bias=True):
 
         m = []
-        if (scale & (scale - 1)) == 0:    # Is scale = 2^n?
+        if (scale & (scale - 1)) == 0:  # Is scale = 2^n?
             for _ in range(int(math.log(scale, 2))):
                 m.append(conv(n_feats, 4 * n_feats, 3, bias))
                 m.append(nn.PixelShuffle(2))
@@ -39,19 +110,12 @@ class Upsampler_han(nn.Sequential):
         else:
             raise NotImplementedError
 
-        super(Upsampler_han, self).__init__(*m)
+        super(Upsampler, self).__init__(*m)
 
-class MeanShift_han(nn.Conv2d):
-    def __init__(
-        self, rgb_range,
-        rgb_mean=(0.4488, 0.4371, 0.4040), rgb_std=(1.0, 1.0, 1.0), sign=-1):
 
-        super(MeanShift_han, self).__init__(3, 3, kernel_size=1)
-        std = torch.Tensor(rgb_std)
-        self.weight.data = torch.eye(3).view(3, 3, 1, 1) / std.view(3, 1, 1, 1)
-        self.bias.data = sign * rgb_range * torch.Tensor(rgb_mean) / std
-        for p in self.parameters():
-            p.requires_grad = False
+def make_model(args, parent=False):
+    return HAN(args)
+
 
 ## Channel Attention (CA) Layer
 class CALayer(nn.Module):
@@ -61,10 +125,10 @@ class CALayer(nn.Module):
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         # feature channel downscale and upscale --> channel weight
         self.conv_du = nn.Sequential(
-                nn.Conv2d(channel, channel // reduction, 1, padding=0, bias=True),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(channel // reduction, channel, 1, padding=0, bias=True),
-                nn.Sigmoid()
+            nn.Conv2d(channel, channel // reduction, 1, padding=0, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channel // reduction, channel, 1, padding=0, bias=True),
+            nn.Sigmoid(),
         )
 
     def forward(self, x):
@@ -72,56 +136,60 @@ class CALayer(nn.Module):
         y = self.conv_du(y)
         return x * y
 
+
 class LAM_Module(nn.Module):
-    """ Layer attention module"""
+    """Layer attention module"""
+
     def __init__(self, in_dim):
         super(LAM_Module, self).__init__()
         self.chanel_in = in_dim
 
-
         self.gamma = nn.Parameter(torch.zeros(1))
-        self.softmax  = nn.Softmax(dim=-1)
-    def forward(self,x):
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
         """
-            inputs :
-                x : input feature maps( B X N X C X H X W)
-            returns :
-                out : attention value + input feature
-                attention: B X N X N
+        inputs :
+            x : input feature maps( B X N X C X H X W)
+        returns :
+            out : attention value + input feature
+            attention: B X N X N
         """
         m_batchsize, N, C, height, width = x.size()
         proj_query = x.view(m_batchsize, N, -1)
         proj_key = x.view(m_batchsize, N, -1).permute(0, 2, 1)
         energy = torch.bmm(proj_query, proj_key)
-        energy_new = torch.max(energy, -1, keepdim=True)[0].expand_as(energy)-energy
+        energy_new = torch.max(energy, -1, keepdim=True)[0].expand_as(energy) - energy
         attention = self.softmax(energy_new)
         proj_value = x.view(m_batchsize, N, -1)
 
         out = torch.bmm(attention, proj_value)
         out = out.view(m_batchsize, N, C, height, width)
 
-        out = self.gamma*out + x
+        out = self.gamma * out + x
         out = out.view(m_batchsize, -1, height, width)
         return out
 
+
 class CSAM_Module(nn.Module):
-    """ Channel-Spatial attention module"""
+    """Channel-Spatial attention module"""
+
     def __init__(self, in_dim):
         super(CSAM_Module, self).__init__()
         self.chanel_in = in_dim
 
-
         self.conv = nn.Conv3d(1, 1, 3, 1, 1)
         self.gamma = nn.Parameter(torch.zeros(1))
-        #self.softmax  = nn.Softmax(dim=-1)
+        # self.softmax  = nn.Softmax(dim=-1)
         self.sigmoid = nn.Sigmoid()
-    def forward(self,x):
+
+    def forward(self, x):
         """
-            inputs :
-                x : input feature maps( B X N X C X H X W)
-            returns :
-                out : attention value + input feature
-                attention: B X N X N
+        inputs :
+            x : input feature maps( B X N X C X H X W)
+        returns :
+            out : attention value + input feature
+            attention: B X N X N
         """
         m_batchsize, C, height, width = x.size()
         out = x.unsqueeze(1)
@@ -137,42 +205,65 @@ class CSAM_Module(nn.Module):
         # out = torch.bmm(attention, proj_value)
         # out = out.view(m_batchsize, N, C, height, width)
 
-        out = self.gamma*out
+        out = self.gamma * out
         out = out.view(m_batchsize, -1, height, width)
         x = x * out + x
         return x
 
+
 ## Residual Channel Attention Block (RCAB)
 class RCAB(nn.Module):
     def __init__(
-        self, conv, n_feat, kernel_size, reduction,
-        bias=True, bn=False, act=nn.ReLU(True), res_scale=1):
+        self,
+        conv,
+        n_feat,
+        kernel_size,
+        reduction,
+        bias=True,
+        bn=False,
+        act=nn.ReLU(True),
+        res_scale=1,
+    ):
 
         super(RCAB, self).__init__()
         modules_body = []
         for i in range(2):
             modules_body.append(conv(n_feat, n_feat, kernel_size, bias=bias))
-            if bn: modules_body.append(nn.BatchNorm2d(n_feat))
-            if i == 0: modules_body.append(act)
+            if bn:
+                modules_body.append(nn.BatchNorm2d(n_feat))
+            if i == 0:
+                modules_body.append(act)
         modules_body.append(CALayer(n_feat, reduction))
         self.body = nn.Sequential(*modules_body)
         self.res_scale = res_scale
 
     def forward(self, x):
         res = self.body(x)
-        #res = self.body(x).mul(self.res_scale)
+        # res = self.body(x).mul(self.res_scale)
         res += x
         return res
 
+
 ## Residual Group (RG)
 class ResidualGroup(nn.Module):
-    def __init__(self, conv, n_feat, kernel_size, reduction, act, res_scale, n_resblocks):
+    def __init__(
+        self, conv, n_feat, kernel_size, reduction, act, res_scale, n_resblocks
+    ):
         super(ResidualGroup, self).__init__()
         modules_body = []
         modules_body = [
             RCAB(
-                conv, n_feat, kernel_size, reduction, bias=True, bn=False, act=nn.ReLU(True), res_scale=1) \
-            for _ in range(n_resblocks)]
+                conv,
+                n_feat,
+                kernel_size,
+                reduction,
+                bias=True,
+                bn=False,
+                act=nn.ReLU(True),
+                res_scale=1,
+            )
+            for _ in range(n_resblocks)
+        ]
         modules_body.append(conv(n_feat, n_feat, kernel_size))
         self.body = nn.Sequential(*modules_body)
 
@@ -181,84 +272,88 @@ class ResidualGroup(nn.Module):
         res += x
         return res
 
+
 ## Holistic Attention Network (HAN)
 @ARCH_REGISTRY.register()
 class HAN(nn.Module):
-    def __init__(self,
-                 n_resblocks,
-                 n_resgroups,
-                 n_feats,
-                 reduction,
-                 scale,
-                 rgb_range,
-                 n_colors,
-                 res_scale,
-                 conv=default_conv):
+    def __init__(
+        self,
+        num_in_ch,
+        num_out_ch,
+        num_feat=64,
+        num_group=10,
+        num_block=20,
+        squeeze_factor=16,
+        upscale=4,
+        res_scale=1,
+        img_range=255.0,
+        rgb_mean=(0.4488, 0.4371, 0.4040),
+    ):
         super(HAN, self).__init__()
-        # n_resgroups = args.n_resgroups
-        # n_resblocks = args.n_resblocks
-        # n_feats = args.n_feats
+        conv = default_conv
+        n_resgroups = num_group
+        n_resblocks = num_block
+        n_feats = num_feat
         kernel_size = 3
-        # reduction = args.reduction
-        scale = scale
+        reduction = squeeze_factor
+        scale = upscale
         act = nn.ReLU(True)
 
         # RGB mean for DIV2K
-        rgb_mean = (0.4488, 0.4371, 0.4040)
         rgb_std = (1.0, 1.0, 1.0)
-        # self.sub_mean = MeanShift_han(args.rgb_range, rgb_mean, rgb_std)
-        self.sub_mean = MeanShift_han(rgb_range, rgb_mean, rgb_std)
+        self.sub_mean = MeanShift(img_range, rgb_mean, rgb_std)
+
         # define head module
-        # modules_head = [conv(args.n_colors, n_feats, kernel_size)]
-        modules_head = [conv(n_colors, n_feats, kernel_size)]
+        modules_head = [conv(num_in_ch, n_feats, kernel_size)]
 
         # define body module
-        # modules_body = [
-        #     ResidualGroup(
-        #         conv, n_feats, kernel_size, reduction, act=act, res_scale=args.res_scale, n_resblocks=n_resblocks) \
-        #     for _ in range(n_resgroups)]
         modules_body = [
             ResidualGroup(
-                conv, n_feats, kernel_size, reduction, act=act, res_scale=res_scale, n_resblocks=n_resblocks) \
-            for _ in range(n_resgroups)]
+                conv,
+                n_feats,
+                kernel_size,
+                reduction,
+                act=act,
+                res_scale=res_scale,
+                n_resblocks=n_resblocks,
+            )
+            for _ in range(n_resgroups)
+        ]
 
         modules_body.append(conv(n_feats, n_feats, kernel_size))
 
         # define tail module
-        # modules_tail = [
-        #     Upsampler_han(conv, scale, n_feats, act=False),
-        #     conv(n_feats, args.n_colors, kernel_size)]
         modules_tail = [
-            Upsampler_han(conv, scale, n_feats, act=False),
-            conv(n_feats, n_colors, kernel_size)]
+            Upsampler(conv, scale, n_feats, act=False),
+            conv(n_feats, num_out_ch, kernel_size),
+        ]
 
-        # self.add_mean = MeanShift_han(args.rgb_range, rgb_mean, rgb_std, 1)
-        self.add_mean = MeanShift_han(rgb_range, rgb_mean, rgb_std, 1)
+        self.add_mean = MeanShift(img_range, rgb_mean, rgb_std, 1)
 
         self.head = nn.Sequential(*modules_head)
         self.body = nn.Sequential(*modules_body)
         self.csa = CSAM_Module(n_feats)
         self.la = LAM_Module(n_feats)
-        self.last_conv = nn.Conv2d(n_feats*11, n_feats, 3, 1, 1)
-        self.last = nn.Conv2d(n_feats*2, n_feats, 3, 1, 1)
+        self.last_conv = nn.Conv2d(n_feats * 11, n_feats, 3, 1, 1)
+        self.last = nn.Conv2d(n_feats * 2, n_feats, 3, 1, 1)
         self.tail = nn.Sequential(*modules_tail)
 
     def forward(self, x):
         x = self.sub_mean(x)
         x = self.head(x)
         res = x
-        #pdb.set_trace()
+        # pdb.set_trace()
         for name, midlayer in self.body._modules.items():
             res = midlayer(res)
-            #print(name)
-            if name=='0':
+            # print(name)
+            if name == '0':
                 res1 = res.unsqueeze(1)
             else:
-                res1 = torch.cat([res.unsqueeze(1),res1],1)
-        #res = self.body(x)
+                res1 = torch.cat([res.unsqueeze(1), res1], 1)
+        # res = self.body(x)
         out1 = res
-        #res3 = res.unsqueeze(1)
-        #res = torch.cat([res1,res3],1)
+        # res3 = res.unsqueeze(1)
+        # res = torch.cat([res1,res3],1)
         res = self.la(res1)
         out2 = self.last_conv(res)
 
@@ -267,35 +362,9 @@ class HAN(nn.Module):
         res = self.last(out)
 
         res += x
-        #res = self.csa(res)
+        # res = self.csa(res)
 
         x = self.tail(res)
         x = self.add_mean(x)
 
         return x
-
-    def load_state_dict(self, state_dict, strict=False):
-        own_state = self.state_dict()
-        for name, param in state_dict.items():
-            if name in own_state:
-                if isinstance(param, nn.Parameter):
-                    param = param.data
-                try:
-                    own_state[name].copy_(param)
-                except Exception:
-                    if name.find('tail') >= 0:
-                        print('Replace pre-trained upsampler to new one...')
-                    else:
-                        raise RuntimeError('While copying the parameter named {}, '
-                                           'whose dimensions in the model are {} and '
-                                           'whose dimensions in the checkpoint are {}.'
-                                           .format(name, own_state[name].size(), param.size()))
-            elif strict:
-                if name.find('tail') == -1:
-                    raise KeyError('unexpected key "{}" in state_dict'
-                                   .format(name))
-
-        if strict:
-            missing = set(own_state.keys()) - set(state_dict.keys())
-            if len(missing) > 0:
-                raise KeyError('missing keys in state_dict: "{}"'.format(missing))
